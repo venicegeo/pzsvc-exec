d890a5ae8b61bd7563fdf1b1d8df05cf494470e0
Filip Sufitchi <filips@imagemattersllc.com>
2018-03-16 14:29:58 -0400

    Update test scripts to include all packages

---
+++ test.bat
@@ -0,0 +1,8 @@
+go test -cover ^
+  github.com/venicegeo/pzsvc-exec/dispatcher ^
+  github.com/venicegeo/pzsvc-exec/pzsvc ^
+  github.com/venicegeo/pzsvc-exec/worker ^
+  github.com/venicegeo/pzsvc-exec/worker/config ^
+  github.com/venicegeo/pzsvc-exec/worker/ingest ^
+  github.com/venicegeo/pzsvc-exec/worker/log ^
+  github.com/venicegeo/pzsvc-exec/worker/workerexec
--- test.sh
+++ test.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 go test -cover \
+  github.com/venicegeo/pzsvc-exec/dispatcher \
+  github.com/venicegeo/pzsvc-exec/pzsvc \
+  github.com/venicegeo/pzsvc-exec/worker \
   github.com/venicegeo/pzsvc-exec/worker/config \
-  github.com/venicegeo/pzsvc-exec/worker/ingest
+  github.com/venicegeo/pzsvc-exec/worker/ingest \
+  github.com/venicegeo/pzsvc-exec/worker/log \
+  github.com/venicegeo/pzsvc-exec/worker/workerexec
