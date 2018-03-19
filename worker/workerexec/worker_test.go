b2897c4c3dd631c223c597319f83aa94d872c225
Filip Sufitchi <filips@imagemattersllc.com>
2018-03-19 17:58:32 -0400

    Complete worker unit tests

--- worker/workerexec/worker_test.go
+++ worker/workerexec/worker_test.go
@@ -2,6 +2,7 @@ package workerexec
 
 import (
 	"errors"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -72,6 +73,7 @@ func TestExec_Success(t *testing.T) {
 		config.InputSource{FileName: "input1.txt", URL: "http://example1.localdomain/input1_source.txt"},
 		config.InputSource{FileName: "input2.tif", URL: "http://example2.localdomain/input2_source.tif"},
 	}
+	execMock.workerConfig.Outputs = []string{"output1.txt", "output2.geojson"}
 	execMock.workerConfig.PzSEConfig.CliCmd = "test cli command"
 	execMock.workerConfig.CLICommandExtra = "--extra"
 	execMock.workerConfig.PzSEConfig.VersionCmd = "version cli command"
@@ -108,7 +110,7 @@ func TestExec_ErrorInputs(t *testing.T) {
 		config.InputSource{FileName: "input2.tif", URL: "http://example2.localdomain/input2_source.tif"},
 	}
 	execMock.worker.fetchInputsFunc = func(cfg config.WorkerConfig, inputs []config.InputSource) error {
-		return errors.New("test error")
+		return errors.New("test input error")
 	}
 
 	// Tested code
@@ -120,5 +122,94 @@ func TestExec_ErrorInputs(t *testing.T) {
 	// check error exec result was sent to piazza, containing our error message
 	assert.Len(t, execMock.sendExecResultDataCalls, 1)
 	assert.Equal(t, pzsvc.PiazzaStatusError, execMock.sendExecResultDataCalls[0].status)
-	assert.Contains(t, string(execMock.sendExecResultDataCalls[0].resultData), "test error")
+	assert.Contains(t, string(execMock.sendExecResultDataCalls[0].resultData), "test input error")
+}
+
+func TestExec_ErrorVersionCmd(t *testing.T) {
+	// Setup
+	execMock := execMockSetup()
+	execMock.workerConfig.PzSEConfig.VersionCmd = "version-cmd"
+	execMock.worker.commandRunner.exec = func(cmdName string, args ...string) ([]byte, error) {
+		for _, arg := range args {
+			if strings.Contains(arg, "version-cmd") {
+				return []byte{}, errors.New("test version cmd error")
+			}
+		}
+		return []byte("ok"), nil
+	}
+
+	// Tested code
+	err := execMock.worker.Exec(*execMock.workerConfig)
+
+	// Asserts
+	assert.Nil(t, err) // check no unrecoverable error
+
+	// check error exec result was sent to piazza, containing our error message
+	assert.Len(t, execMock.sendExecResultDataCalls, 1)
+	assert.Equal(t, pzsvc.PiazzaStatusError, execMock.sendExecResultDataCalls[0].status)
+	assert.Contains(t, string(execMock.sendExecResultDataCalls[0].resultData), "test version cmd error")
+}
+
+func TestExec_ErrorAlgoCmd(t *testing.T) {
+	// Setup
+	execMock := execMockSetup()
+	execMock.workerConfig.PzSEConfig.CliCmd = "algo-cmd"
+	execMock.worker.commandRunner.exec = func(cmdName string, args ...string) ([]byte, error) {
+		for _, arg := range args {
+			if strings.Contains(arg, "algo-cmd") {
+				return []byte{}, errors.New("test algo cmd error")
+			}
+		}
+		return []byte("ok"), nil
+	}
+
+	// Tested code
+	err := execMock.worker.Exec(*execMock.workerConfig)
+
+	// Asserts
+	assert.Nil(t, err) // check no unrecoverable error
+
+	// check error exec result was sent to piazza, containing our error message
+	assert.Len(t, execMock.sendExecResultDataCalls, 1)
+	assert.Equal(t, pzsvc.PiazzaStatusError, execMock.sendExecResultDataCalls[0].status)
+	assert.Contains(t, string(execMock.sendExecResultDataCalls[0].resultData), "test algo cmd error")
+}
+
+func TestExec_ErrorIngest(t *testing.T) {
+	// Setup
+	execMock := execMockSetup()
+	execMock.worker.outputFilesToPiazzaFunc = func(cfg config.WorkerConfig, algFullCommand string, algVersion string) ingest.MultiIngestOutput {
+		return ingest.MultiIngestOutput{
+			CombinedError: errors.New("test combined error"),
+			Errors:        []error{errors.New("test error 1"), errors.New("test error 2")},
+		}
+	}
+
+	// Tested code
+	err := execMock.worker.Exec(*execMock.workerConfig)
+
+	// Asserts
+	assert.Nil(t, err) // check no unrecoverable error
+
+	// check error exec result was sent to piazza, containing our error message
+	assert.Len(t, execMock.sendExecResultDataCalls, 1)
+	assert.Equal(t, pzsvc.PiazzaStatusError, execMock.sendExecResultDataCalls[0].status)
+	assert.Contains(t, string(execMock.sendExecResultDataCalls[0].resultData), "test error 1")
+	assert.Contains(t, string(execMock.sendExecResultDataCalls[0].resultData), "test error 2")
+}
+
+func TestExec_ErrorSendExecResult(t *testing.T) {
+	// Setup
+	execMock := execMockSetup()
+	mockError := &pzsvc.PzCustomError{}
+	execMock.worker.piazzaOutputter.sendExecResultData = func(s pzsvc.Session, pzAddr, svcID, jobID string, status pzsvc.PiazzaStatus, resultData []byte) *pzsvc.PzCustomError {
+		return mockError
+	}
+
+	// Tested code
+	err := execMock.worker.Exec(*execMock.workerConfig)
+
+	// Asserts
+	assert.NotNil(t, err) // check there should be an unrecoverable error
+	assert.Contains(t, err.Error(), "failed to send result data")
 }
