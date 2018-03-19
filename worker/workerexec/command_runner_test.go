a4656a9fc4059e27f6fc74e78452beaafcf41354
Filip Sufitchi <filips@imagemattersllc.com>
2018-03-19 16:05:51 -0400

    Test unknown exec errors and native command execution

--- worker/workerexec/command_runner_test.go
+++ worker/workerexec/command_runner_test.go
@@ -1,6 +1,7 @@
 package workerexec
 
 import (
+	"errors"
 	"os/exec"
 	"testing"
 
@@ -32,7 +33,7 @@ func TestDefaultCommandRunner_Success(t *testing.T) {
 	assert.Equal(t, []string{"sh", "-c", "test command"}, execCalls[0])
 }
 
-func TestDefaultCommandRunner_Error(t *testing.T) {
+func TestDefaultCommandRunner_ExitError(t *testing.T) {
 	// Setup
 	execCalls := [][]string{}
 	exec := func(cmdName string, args ...string) ([]byte, error) {
@@ -53,4 +54,45 @@ func TestDefaultCommandRunner_Error(t *testing.T) {
 	assert.NotNil(t, output.Error)
 	assert.Len(t, execCalls, 1)
 	assert.Equal(t, []string{"sh", "-c", "test command"}, execCalls[0])
+}
+
+func TestDefaultCommandRunner_UnknownError(t *testing.T) {
+	// Setup
+	execCalls := [][]string{}
+	exec := func(cmdName string, args ...string) ([]byte, error) {
+		call := append([]string{cmdName}, args...)
+		execCalls = append(execCalls, call)
+		return []byte("stdout test error"), errors.New("unknown error")
+	}
+	workerConfig := config.WorkerConfig{MuteLogs: true, Session: &pzsvc.Session{}}
+
+	// Tested code
+	runner := newDefaultCommandRunner()
+	runner.exec = exec
+	output := runner.Run(workerConfig, "test command")
+
+	// Asserts
+	assert.Equal(t, []byte("stdout test error"), output.Stdout)
+	assert.Empty(t, output.Stderr)
+	assert.NotNil(t, output.Error)
+	assert.Len(t, execCalls, 1)
+	assert.Equal(t, []string{"sh", "-c", "test command"}, execCalls[0])
+}
+
+func TestDefaultCommandRunner_NativeCommand(t *testing.T) {
+	// Availability probe
+	probeOutput, err := exec.Command("sh", "-c", "echo hello").Output()
+	if err != nil || string(probeOutput) != "hello\n" {
+		t.Skip("`sh -c` not available on this platform")
+	}
+	workerConfig := config.WorkerConfig{MuteLogs: true, Session: &pzsvc.Session{}}
+
+	// Tested code
+	runner := newDefaultCommandRunner()
+	output := runner.Run(workerConfig, "echo hello")
+
+	// Asserts
+	assert.Equal(t, []byte("hello\n"), output.Stdout)
+	assert.Empty(t, output.Stderr)
+	assert.Nil(t, output.Error)
 }
