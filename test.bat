6cb565c9aaa8491aa45c37c5ef88164f225d4170
Filip Sufitchi <filips@imagemattersllc.com>
2018-03-19 14:28:59 -0400

    Include input module in test script

--- test.bat
+++ test.bat
@@ -3,6 +3,7 @@ go test -cover ^
   github.com/venicegeo/pzsvc-exec/pzsvc ^
   github.com/venicegeo/pzsvc-exec/worker ^
   github.com/venicegeo/pzsvc-exec/worker/config ^
+  github.com/venicegeo/pzsvc-exec/worker/input ^
   github.com/venicegeo/pzsvc-exec/worker/ingest ^
   github.com/venicegeo/pzsvc-exec/worker/log ^
   github.com/venicegeo/pzsvc-exec/worker/workerexec
--- test.sh
+++ test.sh
@@ -5,5 +5,6 @@ go test -cover \
   github.com/venicegeo/pzsvc-exec/worker \
   github.com/venicegeo/pzsvc-exec/worker/config \
   github.com/venicegeo/pzsvc-exec/worker/ingest \
+  github.com/venicegeo/pzsvc-exec/worker/input \
   github.com/venicegeo/pzsvc-exec/worker/log \
   github.com/venicegeo/pzsvc-exec/worker/workerexec
