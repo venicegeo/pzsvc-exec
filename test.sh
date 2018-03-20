9fb00b97954b3d5d0eedb03889938f34e20e0d46
Filip Sufitchi <filips@imagemattersllc.com>
2018-03-20 17:27:04 -0400

    Add new dispatcher packages to test scripts

--- test.sh
+++ test.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 go test -cover \
   github.com/venicegeo/pzsvc-exec/dispatcher \
+  github.com/venicegeo/pzsvc-exec/dispatcher/cfwrapper \
+  github.com/venicegeo/pzsvc-exec/dispatcher/model \
+  github.com/venicegeo/pzsvc-exec/dispatcher/poll \
   github.com/venicegeo/pzsvc-exec/pzsvc \
   github.com/venicegeo/pzsvc-exec/worker \
   github.com/venicegeo/pzsvc-exec/worker/config \
